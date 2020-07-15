6be7d591bca4fff112fe126221946ae391da243b
Khaled FERJANI <kferjani@linagora.com>
2020-07-15 15:26:02 +0100

    #3 fix template import

--- src/linagora.esn.admin/app/app.run.js
+++ src/linagora.esn.admin/app/app.run.js
@@ -2,12 +2,21 @@
 
 angular.module('linagora.esn.admin')
 
-.run(function(dynamicDirectiveService, session) {
-  session.ready.then(function() {
-    if (session.userIsDomainAdministrator() || session.user.isPlatformAdmin) {
-      var admin = new dynamicDirectiveService.DynamicDirective(true, 'admin-application-menu', { priority: -10 });
+  .run(addEsnApplicationMenuDirective)
+  .run(addTemplateCache);
 
-      dynamicDirectiveService.addInjection('esn-application-menu', admin);
-    }
-  });
-});
+  function addEsnApplicationMenuDirective(dynamicDirectiveService, session) {
+    session.ready.then(function() {
+      if (session.userIsDomainAdministrator() || session.user.isPlatformAdmin) {
+        var admin = new dynamicDirectiveService.DynamicDirective(true, 'admin-application-menu', { priority: -10 });
+
+        dynamicDirectiveService.addInjection('esn-application-menu', admin);
+      }
+    });
+  }
+
+  function addTemplateCache($templateCache) {
+    $templateCache.put('/admin/app/sidebar/admin-aside.html', require('./sidebar/admin-aside.pug'));
+    $templateCache.put('/admin/app/users/states/edit/admin-users-states-edit.html', require('./users/states/edit/admin-users-states-edit.pug'));
+    $templateCache.put('/views/modules/auto-complete/attendees.html', require('esn-frontend-common-libs/src/frontend/views/modules/auto-complete/attendees.pug'));
+  }
