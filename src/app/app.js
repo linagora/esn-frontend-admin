4f3edb1dc77e8032d3032543a215dbab39014e77
Christophe HAMERLING <chamerling@linagora.com>
2020-08-26 15:09:27 +0200

    OpenPaaS-Suite/esn#20 Add auth switch support

--- src/app/app.js
+++ src/app/app.js
@@ -2,11 +2,13 @@ angular.module('esnApp', [
   'ui.router',
   'linagora.esn.admin',
   'esn.session',
+  'esn.login',
   'esn.websocket'
 ]);
 
 require('esn-frontend-common-libs/src/frontend/js/modules/session');
 require('esn-frontend-common-libs/src/frontend/js/modules/websocket');
+require('esn-frontend-common-libs/src/frontend/js/modules/login');
 
 require('./app.config');
 require('./app.run');
